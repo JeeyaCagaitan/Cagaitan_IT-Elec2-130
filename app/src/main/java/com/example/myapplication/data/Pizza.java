3c884015b9d38c0f0b303596fa734620a12d1884
JeeyaCagaitan <jeeyakarish@gmail.com>
2023-03-11 18:47:31 +0800

    Fragments

---
+++ app/src/main/java/com/example/myapplication/data/Pizza.java
@@ -0,0 +1,31 @@
+package com.example.myapplication.data;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Pizza#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Pizza {
+
+    private static String[] pizzaMenu = {
+            "ALL MEAT PIZZA",
+            "CALIFORNIA VEGGIE",
+            "GARLIC CHICKEN"
+    };
+    private static String[] pizzaDetails = {
+            "Premium Crushed Tomato Sauce made of 100% California...",
+            "Premium crushed tomato sauce, fresh green peppers, fresh...",
+            "Creamy garlic Parmesan sauce, grilled chicken, applemood..."
+    };
+}
+
