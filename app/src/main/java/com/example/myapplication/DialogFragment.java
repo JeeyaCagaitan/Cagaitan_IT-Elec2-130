c35067750c41e51f2122fb9e0af0bc674034ae91
JeeyaCagaitan <jeeyakarish@gmail.com>
2023-03-18 18:23:54 +0800

    UI components

--- app/src/main/java/com/example/myapplication/DialogFragment.java
+++ app/src/main/java/com/example/myapplication/DialogFragment.java
@@ -18,6 +18,6 @@ public class DialogFragment extends androidx.fragment.app.DialogFragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.fragment_dialog, container, false);
+        return inflater.inflate(R.layout.fragment_dialog3, container, false);
     }
 }
